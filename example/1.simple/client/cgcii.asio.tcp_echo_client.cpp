654ae153c13087a5139e2e41ca7adcc64518d627
sangduck <sangducks@gmail.com>
2023-07-14 01:10:40 +0900

    commit

---
+++ example/1.simple/client/cgcii.asio.tcp_echo_client.cpp
@@ -0,0 +1,37 @@
+﻿#include "cgcii.asio/asio.h"
+#include <thread>
+#if defined(_WIN32)
+	#include <conio.h>
+#elif defined(__linux__)
+	int _kbhit();
+	int _getch();
+#endif
+
+
+class socket_tcp : public asio::Nsocket_tcp_client
+{
+public:
+	virtual void on_connect() override
+	{
+	}
+	virtual void on_disconnect(boost::system::error_code /*_error_code*/) noexcept override
+	{
+	}
+	virtual int on_message(shared_buffer& _msg) override
+	{
+		send(_msg);
+		return 1;
+	}
+};
+
+int main()
+{
+	// 1) create socket
+	auto psocket_tcp = std::make_shared<socket_tcp>();
+
+	// 2) start(connect)
+	psocket_tcp->start(boost::asio::ip::tcp::endpoint{ boost::asio::ip::address_v4::loopback(), 20000 });
+
+	// 3) wait for exit pressing ESC key
+	while (_getch() != 27);
+}
---
+++ example/1.simple/server/cgcii.asio.tcp_echo_server.cpp
@@ -0,0 +1,35 @@
+﻿#include "cgcii.asio/asio.h"
+#if defined(_WIN32)
+	#include <conio.h>
+#elif defined(__linux__)
+	int _getch();
+#endif
+
+
+class socket_tcp : public asio::Nsocket_tcp_gather_buffer
+{
+public:
+	virtual void on_connect() override
+	{
+	}
+	virtual void on_disconnect(boost::system::error_code /*_error_code*/) noexcept override
+	{
+	}
+	virtual int on_message(shared_buffer& _msg) override
+	{
+		send(_msg);
+		return 1;
+	}
+};
+
+int main()
+{
+	// 1) create acceptor
+	auto pacceptor = std::make_shared<asio::acceptor<socket_tcp>>();
+
+	// 2) start accept
+	pacceptor->start(tcp::endpoint{ boost::asio::ip::tcp::v6(), 20000 });
+
+	// 3) wait for exit pressing ESC key
+	while (_getch() != 27);
+}
